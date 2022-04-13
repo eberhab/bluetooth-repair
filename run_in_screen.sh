d8946427dbca924e741abd728778fdc4d4cefe5e
eberhab <eberhab@gmail.com>
2022-04-13 18:37:15 +0200

    Fix issue with log file name

--- run_in_screen.sh
+++ run_in_screen.sh
@@ -7,7 +7,7 @@
 if ! screen -list | grep -q "bluetooth"; then
   screen -a -A -d -m -S bluetooth -t bash bash
   screen -S bluetooth -X screen -t syslog tail -F /var/log/syslog
-  screen -S bluetooth -X screen -t bt-log tail -F ~/bluetooth-repair/bluetooth-repair.log
+  screen -S bluetooth -X screen -t bt-log tail -F ~/bluetooth-repair/bt-repair.log
   screen -S bluetooth -X screen -t bt-repair bash
   screen -S bluetooth -p bt-repair -X stuff $'cd ~/bluetooth-repair\nwhile true; do ./bt-repair.sh; sleep 5; done\n\n'
 fi
